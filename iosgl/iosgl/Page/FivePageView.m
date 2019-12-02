e508c655c0c1b2b67273fab9d4b70434d569dc78
zhaojianeng25 <zhaojianeng25@gmail.com>
2019-12-03 00:15:45 +0700

    cat

--- iosgl/iosgl/Page/FivePageView.m
+++ iosgl/iosgl/Page/FivePageView.m
@@ -20,17 +20,43 @@
     self.view.backgroundColor=[UIColor redColor];
     [self.view addSubview:self.scrolView];
     
+    [self addScrollView];
+    
     
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+-(void)addScrollView{
+    
+    _scrolView.bounces=NO;
+   // _scrolView.userInteractionEnabled=YES;
+    //是否按整页来滚动视图
+    _scrolView.pagingEnabled=YES;
+    _scrolView.contentSize =CGSizeMake(_scrolView.bounds.size.width*2, _scrolView.bounds.size.height);
+    _scrolView.showsHorizontalScrollIndicator = NO;//水平滚动条
+    _scrolView.showsVerticalScrollIndicator = NO;
+   // _scrolView.delegate=self;
+    
+       _scrolView.frame= self.view.frame;
+    
+    
+    [self addPublicTabelView];
+    [self addPrivteTabelView];
+    
 }
-*/
+-(void)addPublicTabelView
+{
+    UIImageView *tempui=[[UIImageView alloc]init];
+    tempui.image=[UIImage imageNamed:@"xinshoupic"];
+    tempui.frame=_scrolView.bounds;
+    [_scrolView addSubview:tempui];
+    
+}
+-(void)addPrivteTabelView
+{
+    UIImageView *tempui=[[UIImageView alloc]init];
+      tempui.image=[UIImage imageNamed:@"xinshoupic"];
+      tempui.frame=CGRectMake(_scrolView.bounds.size.width, 0, _scrolView.bounds.size.width, _scrolView.bounds.size.height*1.5);
+      [_scrolView addSubview:tempui];
+}
+ 
 
 @end
