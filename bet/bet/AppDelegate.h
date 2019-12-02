67a8d54dc5f44516c3becf44b239d49d32be3af9
zhaojianeng25 <zhaojianeng25@gmail.com>
2019-12-02 16:18:20 +0700

    ceee

---
+++ bet/bet/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  bet
+//
+//  Created by zhao on 2/12/2019.
+//  Copyright © 2019 zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
