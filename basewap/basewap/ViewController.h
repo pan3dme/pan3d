e70769933c4bfc1bce558dc943fb2db04b597ff0
zhaojianeng25 <zhaojianeng25@gmail.com>
2019-12-02 16:25:10 +0700

    tttt

---
+++ basewap/basewap/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  basewap
+//
+//  Created by zhao on 2/12/2019.
+//  Copyright © 2019 zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ basewap/basewap/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  basewap
+//
+//  Created by zhao on 2/12/2019.
+//  Copyright © 2019 zhao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
