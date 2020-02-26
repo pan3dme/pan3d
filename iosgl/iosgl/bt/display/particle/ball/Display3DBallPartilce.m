7e79ace7360b77aabd452c920cb347583a37ad72
赵佳能 <zhaojianeng25@gmail.com>
2020-02-26 16:46:29 +0700

    绘制正方形

--- iosgl/iosgl/bt/display/particle/ball/Display3DBallPartilce.m
+++ iosgl/iosgl/bt/display/particle/ball/Display3DBallPartilce.m
@@ -24,18 +24,22 @@
 -(void)onCreated;
 {
     self.objData=[[ObjData alloc]init];
-    GLfloat attrArr[9];
-    attrArr[0]=0;
-    attrArr[1]=0;
-    attrArr[2]=0;
-    
-    attrArr[3]=1;
-    attrArr[4]=1;
-    attrArr[5]=0;
-    
-    attrArr[6]=1;
-    attrArr[7]=0;
-    attrArr[8]=0;
+    GLfloat attrArr[12];
+attrArr[0]=0.0f;
+attrArr[1]=0.0f;
+attrArr[2]=0.0f;
+
+attrArr[3]=0.25f;
+attrArr[4]=0.0f;
+attrArr[5]=0.0f;
+
+attrArr[6]=0.25f;
+attrArr[7]=0.25f;
+attrArr[8]=0.0f;
+
+attrArr[9]=0.0f;
+attrArr[10]=0.25f;
+attrArr[11]=0.0f;
     
     GLuint verticesBuffer;
     glGenBuffers(1, &verticesBuffer);
@@ -43,10 +47,13 @@
     glBufferData(GL_ARRAY_BUFFER, sizeof(attrArr), attrArr, GL_DYNAMIC_DRAW);
     self.objData.verticesBuffer=verticesBuffer;
     
-    unsigned int Indices[3];
+    unsigned int Indices[6];
     Indices[0]=0;
     Indices[1]=1;
     Indices[2]=2;
+    Indices[3]=0;
+    Indices[4]=2;
+    Indices[5]=3;
     GLuint indexBuffer;
     glGenBuffers(1, &indexBuffer);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
@@ -69,11 +76,11 @@
        // [context3D setVaOffset:self.shader3d name:"position" dataWidth:3 stride:0 offset:0];
         
         GLuint position = glGetAttribLocation( self.shader3d.program,"position");
-          glEnableVertexAttribArray(position);
-          glVertexAttribPointer(position, 3, GL_FLOAT, GL_FALSE,0, (GLfloat *)NULL+0);
+        glEnableVertexAttribArray(position);
+        glVertexAttribPointer(position, 3, GL_FLOAT, GL_FALSE,0, (GLfloat *)NULL+0);
         
         
-        [context3D drawCall:self.objData.indexBuffer  numTril:3 ];
+        [context3D drawCall:self.objData.indexBuffer  numTril:6 ];
     }
     
 }
