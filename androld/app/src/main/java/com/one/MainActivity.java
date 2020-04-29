4d91fa3dc5cea687ca80b68a7d98a5307931172f
赵佳能 <zhaojianeng25@gmail.com>
2020-04-30 03:30:02 +0700

    3二

--- androld/app/src/main/java/com/one/MainActivity.java
+++ androld/app/src/main/java/com/one/MainActivity.java
@@ -24,6 +24,8 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         GLSurfaceView glSurfaceView = new GLSurfaceView(this);
         glSurfaceView.setRenderer(new OpenGLRender());
         setContentView(glSurfaceView);
