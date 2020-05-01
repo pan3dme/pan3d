88eabb891396bc6d6e2519486bcf1f857b57bd88
赵佳能 <zhaojianeng25@gmail.com>
2020-05-02 04:38:16 +0700

    333

--- androld/app/src/main/java/com/one/five/obj/Obj3D.java
+++ androld/app/src/main/java/com/one/five/obj/Obj3D.java
@@ -2,11 +2,13 @@ package com.one.five.obj;
 
 import java.nio.FloatBuffer;
 
+import z3d.base.ObjData;
+
 /**
  * Created by wuwang on 2017/2/22
  */
 
-public class Obj3D {
+public class Obj3D extends ObjData {
     public FloatBuffer vert;
     public int vertCount;
 
