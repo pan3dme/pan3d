4a3e9c0bf1b4a172eaa2ef6b236fb969cc62a1d3
赵佳能 <zhaojianeng25@gmail.com>
2020-05-22 23:42:57 +0700

    3

--- fluttersrc/flutter_app_two/lib/src/base/BaseDataVo.dart
+++ fluttersrc/flutter_app_two/lib/src/base/BaseDataVo.dart
@@ -1,22 +1,43 @@
 import 'dart:math';
 
+/*
+支付方式
+ */
 class ZhifuFanshiVo {
+  /*
+  支付方式名字   「支付宝001」 「微信001」
+   */
   String tittle;
+  /*
+  支付档次  金额 和 其它」
+   */
   List<String> playList;
-  ZhifuFanshiVo(str) {
-    tittle = str;
+  ZhifuFanshiVo(String value) {
+    tittle = value;
     playList = new List();
+    /*
+    随机生成支付金额档次
+     */
     var random = new Random();
     int len=random.nextInt(10)+2;
-    for(int i=0;i<len;i++){
+    for(int i=1;i<len;i++){
       playList.add("$i 0元");
     }
   }
 }
+/*
+支付通道 对应的类型
+ */
 
 class ZhifuLieTongDaoVo {
+  /*
+  当前选取的索引
+   */
   int selectIdx = 0;
-  String tabName = '1';
+  /*
+  支付通道名字    支付宝 微信 人工
+   */
+  String tabName;
   List<ZhifuFanshiVo> menuLists;
   ZhifuLieTongDaoVo(str) {
     this.tabName = str;
@@ -27,6 +48,9 @@ class BaseDataItem {
   int tabIdx = 0;
   List<ZhifuLieTongDaoVo> pageList;
   BaseDataItem() {
+    /*
+    人工创建基础机构数据
+    */
     this.pageList = new List();
     this.pageList.add(makeTab0());
     this.pageList.add(makeTab1());
