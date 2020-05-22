ec31217fb0661ea8ddc9a1dd5a74f97bf58b48a7
赵佳能 <zhaojianeng25@gmail.com>
2020-05-22 22:10:32 +0700

    12

--- fluttersrc/flutter_app_two/lib/src/base/BaseDataVo.dart
+++ fluttersrc/flutter_app_two/lib/src/base/BaseDataVo.dart
@@ -2,10 +2,7 @@ class BaseDataVo {
   int selectIdx = 0;
   String tabName = '1';
   List<String> menuLists = [
-    "支付宝0",
-    "支付宝1",
-    "支付宝2",
-    "支付宝3",
+    "空",
   ];
   BaseDataVo(str) {
     this.tabName = str;
@@ -14,10 +11,47 @@ class BaseDataVo {
 
 class BaseDataItem {
   int tabIdx = 0;
-  List<BaseDataVo> pageList = [
-    new BaseDataVo('微信0'),
-    new BaseDataVo('微信1'),
-    new BaseDataVo('微信2'),
-    new BaseDataVo('微信3'),
-  ];
+  List<BaseDataVo> pageList;
+  BaseDataItem() {
+    this.pageList = new List();
+    this.pageList.add(makeTab0());
+    this.pageList.add(makeTab1());
+    this.pageList.add(makeTab2());
+    this.pageList.add(new BaseDataVo('云闪付'));
+  }
+  makeTab0() {
+    BaseDataVo one = new BaseDataVo('支付宝');
+    one.menuLists = new List();
+    one.menuLists.add("支付宝0");
+    one.menuLists.add("支付宝1");
+    one.menuLists.add("支付宝2");
+    one.menuLists.add("支付宝3");
+    one.menuLists.add("支付宝4");
+    one.menuLists.add("支付宝5");
+    one.menuLists.add("支付宝6");
+
+    return one;
+  }
+
+  makeTab1() {
+    BaseDataVo one = new BaseDataVo('微信');
+    one.menuLists = new List();
+    one.menuLists.add("微信0");
+    one.menuLists.add("微信1");
+    one.menuLists.add("微信2");
+    one.menuLists.add("微信3");
+
+    return one;
+  }
+
+  makeTab2() {
+    BaseDataVo one = new BaseDataVo('人工充值');
+    one.menuLists = new List();
+    one.menuLists.add("人工充值0");
+    one.menuLists.add("人工充值1");
+    one.menuLists.add("人工充值2");
+    one.menuLists.add("人工充值3");
+
+    return one;
+  }
 }
