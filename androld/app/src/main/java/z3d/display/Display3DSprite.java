4a8dfec27f676fd578ea472c9e2d40b1c510bc7e
赵佳能 <zhaojianeng25@gmail.com>
2020-05-23 14:48:14 +0700

    youhu

--- androld/app/src/main/java/z3d/display/Display3DSprite.java
+++ androld/app/src/main/java/z3d/display/Display3DSprite.java
@@ -1,15 +1,20 @@
 package z3d.display;
 
 import android.opengl.GLES20;
-
+import android.util.Log;
 
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 import z3d.base.ObjData;
 import z3d.core.Context3D;
 
+import z3d.material.DynamicTexItem;
+import z3d.material.Material;
+import z3d.material.MaterialBaseParam;
+import z3d.material.TexItem;
 import z3d.program.ProgrmaManager;
 import z3d.program.Shader3D;
 import z3d.vo.Matrix3D;
@@ -117,7 +122,68 @@ public   class Display3DSprite extends Display3D {
 
 
     }
+    protected void setMaterialTexture(Material material, MaterialBaseParam mp)
+    {
+        Context3D ctx=this.scene3d.context3D;
+        List<TexItem> texVec= mp.material.texList;
+        TexItem texItem;
+        for (int i   = 0; i < texVec.size(); i++) {
+            texItem=texVec.get(i);
+            if (texItem.isDynamic) {
+                continue;
+            }
+            if (texItem.type == TexItem.LIGHTMAP) {
+            }
+            else if (texItem.type == TexItem.LTUMAP   ) {
+            }
+            else if (texItem.type == TexItem.CUBEMAP) {
+                Log.d(TAG, "setMaterialTexture: ");
+            }
+            else if (texItem.type == 0) {
+                ctx.setRenderTexture(material.shader,texItem.name,texItem.textureRes.textTureInt,texItem.id);
+            }
+        }
+        List<DynamicTexItem> texDynamicVec  =  mp.dynamicTexList;
 
+
+/*
+ NSArray<TexItem*>* texVec  = mp.material.texList;
+    TexItem* texItem;
+    for (int i   = 0; i < texVec.count; i++) {
+        texItem=texVec[i];
+        if (texItem.isDynamic) {
+            continue;
+        }
+        if (texItem.type == TexItem.LIGHTMAP) {
+        }
+        else if (texItem.type == TexItem.LTUMAP && [Scene_data default].pubLut ) {
+            NSLog(@"TexItem.LTUMAP)");
+        }
+        else if (texItem.type == TexItem.CUBEMAP) {
+            if (material.useDynamicIBL) {// && _reflectionTextureVo) {
+                NSLog(@"TexItem.useDynamicIBL)");
+            } else {
+                if([Scene_data default].skyCubeTexture){
+                    [ctx setRenderTextureCube:material.shader name:texItem.name texture:[Scene_data default].skyCubeTexture level:texItem.id];
+                }
+            }
+        }
+        else if (texItem.type == 0) {
+            [ctx setRenderTexture:material.shader name:texItem.name texture:  texItem.textureRes.textTureLuint level:texItem.id];
+
+        }
+    }
+    NSArray<DynamicTexItem*>* texDynamicVec  =( NSArray<DynamicTexItem*>*) mp.dynamicTexList;
+    for (int i   = 0; i < texDynamicVec.count; i++) {
+        texItem=texDynamicVec[i].target;
+        if(texItem ){
+            [ctx setRenderTexture:material.shader name:texItem.name  texture:texDynamicVec[i].textureRes.textTureLuint level:texItem.id];
+
+
+        }
+    }
+ */
+    }
 
 
 }
--- androld/app/src/main/java/z3d/material/TexItem.java
+++ androld/app/src/main/java/z3d/material/TexItem.java
@@ -1,6 +1,9 @@
 package z3d.material;
 
+import z3d.vo.Vector3D;
+
 public class TexItem {
+
 
     public int id;
     public String url;
@@ -14,4 +17,11 @@ public class TexItem {
     public int  wrap;
     public  int filter;
     public  int mipmap;
+
+    public static int LIGHTMAP = 1;
+    public static int LTUMAP = 2;
+    public static int CUBEMAP = 3;
+    public static int HEIGHTMAP = 4;
+    public static int REFRACTIONMAP = 5;
+
 }
