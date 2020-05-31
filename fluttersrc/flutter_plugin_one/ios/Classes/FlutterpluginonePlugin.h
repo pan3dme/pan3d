37577d4d42717136921fe770453ba9a9fe1378b6
赵佳能 <zhaojianeng25@gmail.com>
2020-06-01 03:15:34 +0700

    2

---
+++ fluttersrc/flutter_plugin_one/ios/Classes/FlutterpluginonePlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface FlutterpluginonePlugin : NSObject<FlutterPlugin>
+@end
---
+++ fluttersrc/flutter_plugin_one/ios/Classes/FlutterpluginonePlugin.m
@@ -0,0 +1,20 @@
+#import "FlutterpluginonePlugin.h"
+
+@implementation FlutterpluginonePlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"flutterpluginone"
+            binaryMessenger:[registrar messenger]];
+  FlutterpluginonePlugin* instance = [[FlutterpluginonePlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
