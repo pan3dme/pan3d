ff5c755612dcb2b0b95a151b853da6bef5de8112
赵佳能 <zhaojianeng25@gmail.com>
2020-06-02 16:27:38 +0700

    33

--- fluttersrc/flutter_app_1/ios/Runner/AppDelegate.swift
+++ fluttersrc/flutter_app_1/ios/Runner/AppDelegate.swift
@@ -10,4 +10,47 @@ import Flutter
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+    override func application(_application:UIApplication, open url:URL, sourceApplication:String?, annotation:Any) ->Bool{
+
+     if(url.host=="safepay")//支付宝支付
+
+            {
+
+     return FlutterAlipayPlugin.handleOpen(url);
+
+            }
+
+     return true;
+
+        }
+
+     override func application(_application:UIApplication, handleOpen url:URL) ->Bool{
+
+    if(url.host=="safepay")//支付宝支付
+
+            {
+
+     return FlutterAlipayPlugin.handleOpen(url);
+
+            }
+
+     return true;
+
+        }
+
+     override func application(_app:UIApplication, open url:URL, options: [UIApplication.OpenURLOptionsKey:Any] = [:]) ->Bool{
+
+    if(url.host=="safepay")//支付宝支付
+
+            {
+
+     return FlutterAlipayPlugin.handleOpen(url);
+
+            }
+
+     return true;
+
+        }
+
+ 
 }
