a7f2e44d3d80ff3fe0cd333bd950c40c826a0f44
赵佳能 <zhaojianeng25@gmail.com>
2020-10-14 16:25:37 +0700

    no message

--- android/SceneWorld001/app/src/main/java/scene/SceneLyfBase.java
+++ android/SceneWorld001/app/src/main/java/scene/SceneLyfBase.java
@@ -4,6 +4,7 @@ import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -27,6 +28,7 @@ import z3d.base.ByteArray;
 import z3d.base.CallBackFun;
 import z3d.base.GroupBackFun;
 import z3d.base.GroupItem;
+import z3d.base.Object3D;
 import z3d.base.Scene_data;
 import z3d.display.BuildDisplay3DSprite;
 import z3d.display.basedis.DisplayTestSprite;
@@ -40,10 +42,13 @@ import z3d.res.GroupRes;
 import z3d.res.SceneRes;
 import z3d.scene.Scene3D;
 import z3d.units.LoaderThread;
+import z3d.units.TimeUtil;
+import z3d.vo.Vector2D;
 
-public class SceneLyfBase extends AppCompatActivity implements View.OnClickListener {
+public class SceneLyfBase extends AppCompatActivity  {
 
 
+    private static final String TAG ="SceneLyfBase" ;
     private GLSurfaceView mGLView;
     private Scene3D scene3D;
     @Override
@@ -51,13 +56,7 @@ public class SceneLyfBase extends AppCompatActivity implements View.OnClickListe
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.scene001_layout);
-
-        LinearLayout layout = (LinearLayout) findViewById(R.id.container);
-
-         mGLView =   findViewById(R.id.mGLView);
-
-
-        GLSurfaceView bt = new GLSurfaceView(getApplicationContext());
+        mGLView =   findViewById(R.id.mGLView);
 
 
         mGLView.setEGLContextClientVersion(2);
@@ -75,6 +74,7 @@ public class SceneLyfBase extends AppCompatActivity implements View.OnClickListe
                 scene3D.camera3D.fovw = width;
                 scene3D.camera3D.fovh = height;
                 scene3D.resizeScene();
+                scene3D.camera3D.distance=100;
             }
 
             @Override
@@ -82,16 +82,13 @@ public class SceneLyfBase extends AppCompatActivity implements View.OnClickListe
 
                 GLES20.glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
                 GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
-                scene3D.camera3D.distance=100;
-//                scene3D.camera3D.rotationY++;
+
                 scene3D.upFrame();
 
 
             }
         });
         mGLView.setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
-
-
         this.addButs("播放", new CallBackFun() {
             @Override
             public void StateChange(boolean State) {
@@ -104,6 +101,20 @@ public class SceneLyfBase extends AppCompatActivity implements View.OnClickListe
             public void StateChange(boolean State) {
                 ParticleManager particleManager=scene3D.particleManager;
                 particleManager.clearAll();
+            }
+        });
+
+        this.addButs("放大", new CallBackFun() {
+            @Override
+            public void StateChange(boolean State) {
+                scene3D.camera3D.distance*=0.8;
+            }
+        });
+
+        this.addButs("缩小", new CallBackFun() {
+            @Override
+            public void StateChange(boolean State) {
+                scene3D.camera3D.distance/=0.8;
             }
         });
     }
@@ -135,7 +146,8 @@ public class SceneLyfBase extends AppCompatActivity implements View.OnClickListe
 //        this.scene3D.addDisplay(new DisplayTestSprite(this.scene3D));
 
     }
-    private void   playLyf()
+
+        private void   playLyf()
     {
         String url="model/levelup_lyf.txt";
         GroupDataManager.getInstance().getGroupData(url, new GroupBackFun() {
@@ -159,9 +171,55 @@ public class SceneLyfBase extends AppCompatActivity implements View.OnClickListe
         });
     }
 
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        int events[] = {MotionEvent.ACTION_DOWN, MotionEvent.ACTION_MOVE,
+                MotionEvent.ACTION_UP, MotionEvent.ACTION_MOVE, MotionEvent.ACTION_CANCEL, MotionEvent.ACTION_OUTSIDE,
+                MotionEvent.ACTION_POINTER_DOWN,MotionEvent.ACTION_POINTER_UP,
+                MotionEvent.EDGE_TOP,MotionEvent.EDGE_BOTTOM,MotionEvent.EDGE_LEFT,MotionEvent.EDGE_RIGHT};
 
-    @Override
-    public void onClick(View v) {
+        String szEvents[]={"ACTION_DOWN", "ACTION_MOVE",
+                "ACTION_UP", "ACTION_MOVE", "ACTION_CANCEL", "ACTION_OUTSIDE",
+                "ACTION_POINTER_DOWN","ACTION_POINTER_UP",
+                "EDGE_TOP","EDGE_BOTTOM","EDGE_LEFT","EDGE_RIGHT"};
+        for(int i=0; i < events.length; i++)
+        {
+            if(events[i] == event.getAction())
+            {
+                switch (szEvents[i]){
+                    case "ACTION_DOWN":
+                        _downPosV2d=new Vector2D( event.getX(), event.getY());
+                        _oldPosV2d=new Object3D();
+                        _oldPosV2d.x=scene3D.camera3D.x;
+                        _oldPosV2d.y=scene3D.camera3D.y;
+                        _oldPosV2d.z=scene3D.camera3D.z;
+                        _oldPosV2d.rotationX=scene3D.camera3D.rotationX;
+                        _oldPosV2d.rotationY=scene3D.camera3D.rotationY;
 
+                        break;
+                    case "ACTION_MOVE":
+                        if(_downPosV2d!=null){
+                         Vector2D toV2d=   new Vector2D( event.getX(), event.getY());
+                         scene3D.camera3D.rotationY= _oldPosV2d.rotationY+(toV2d.x-_downPosV2d.x);
+                         scene3D.camera3D.rotationX= _oldPosV2d.rotationX+(toV2d.y-_downPosV2d.y)/10.0f;
+
+                            Log.d(TAG+ TimeUtil.getTimer(), "滑动吧");
+                        }
+                        break;
+                    case "ACTION_UP":
+                        _downPosV2d=null;
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            }
+        }
+        return super.onTouchEvent(event);
     }
+    Vector2D _downPosV2d;
+    Object3D _oldPosV2d;
+
+
+
 }
--- android/SceneWorld001/app/src/main/java/z3d/display/particle/locus/Display3DLocusShader.java
+++ android/SceneWorld001/app/src/main/java/z3d/display/particle/locus/Display3DLocusShader.java
@@ -38,25 +38,43 @@ public class Display3DLocusShader extends Shader3D {
         return vertex;
     }
     /*
-    void main(void){
-   vec2 tempv0 = v2TexCoord;
-   tempv0.x -= vcmat[3][0].x;
-   float alpha = tempv0.x/vcmat[3][0].y;
-   alpha = 1.0 - clamp(abs(alpha),0.0,1.0);
-   float kill = -tempv0.x;
-   kill *= tempv0.x - vcmat[3][0].z;
-   v2 = vec4(kill,0.0,0.0,alpha);
-   v1 = v2TexCoord;
-   v0 = tempv0;
-   vec4 tempPos = vcmat[2] * v3Position;
-   vec3 mulPos = vec3(tempPos.x,tempPos.y,tempPos.z);
-   vec3 normals = vec3(v3Normal.x,v3Normal.y,v3Normal.z);
-   mulPos = normalize(vec3(vcmat[3][1].xyz) - mulPos);
-   mulPos = cross(mulPos, normals);
-   mulPos = normalize(mulPos);
-   mulPos *= v3Normal.w;
-   tempPos.xyz = mulPos.xyz + v3Position.xyz;
-   gl_Position = vcmat[0]  * vcmat[1] * vcmat[2] * tempPos;
+   char* relplayChat =
+    "attribute vec3 v3Position;\n"
+    "attribute vec2 v2TexCoord;\n"
+    "attribute vec4 v3Normal;\n"
+    "uniform mat4 viewMatrix;\n"
+    "uniform mat4 camMatrix;\n"
+    "uniform mat4 modeMatrix;\n"
+    "uniform vec4 vcmat30;\n"
+    "uniform vec4 vcmat31;\n"
+    "varying vec2 v0;\n"
+    "varying vec2 v1;\n"
+    "varying vec4 v2;\n"
+    "void main()"
+    "{"
+
+        "vec2 tempv0 = v2TexCoord;\n"
+        "tempv0.x -= vcmat30.x;\n"
+        "float alpha = tempv0.x/vcmat30.y;\n"
+        "alpha = 1.0 - clamp(abs(alpha),0.0,1.0);\n"
+        "float kill = -tempv0.x;\n"
+        "kill *= tempv0.x - vcmat30.z;\n"
+        "v2 = vec4(kill,0.0,0.0,alpha);\n"
+        "v1 = v2TexCoord;\n"
+        "v0 = tempv0;\n"
+
+        "vec4 tempPos = modeMatrix * vec4(v3Position.xyz,1.0);\n"
+        "vec3 mulPos = vec3(tempPos.x,tempPos.y,tempPos.z);\n"
+        "vec3 normals = vec3(v3Normal.x,v3Normal.y,v3Normal.z);\n"
+        "mulPos = normalize(vec3(vcmat31.xyz) - mulPos);\n"
+        "mulPos = cross(mulPos, normals);\n"
+        "mulPos = normalize(mulPos);\n"
+        "mulPos *= v3Normal.w*1.0  ;\n"
+        "tempPos.xyz = mulPos.xyz + v3Position.xyz;\n"
+
+        "gl_Position = tempPos*modeMatrix* camMatrix* viewMatrix;\n"
+
+    "}";
 }
      */
     public String getFragmentShaderString() {
