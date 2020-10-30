2bdc1208008a354b3541c42d1d2313f1ab97f1e8
赵佳能 <zhaojianeng25@gmail.com>
2020-10-31 01:57:05 +0700

    no message

--- iosgl/iosgl/Page/sceneview/SceneAllMenu.m
+++ iosgl/iosgl/Page/sceneview/SceneAllMenu.m
@@ -46,10 +46,7 @@
     [arr addObject:@"特效"];
     [arr addObject:@"技能"];
     [arr addObject:@"挂件"];
-    [arr addObject:@"清理"];
-    [arr addObject:@"网格"];
-    [arr addObject:@"拉+"];
-    [arr addObject:@"推-"];
+    [self addBaseMenuButs:arr];
     [self addButsByArr:arr  action: @selector(addMenuListClikEvent:)];
     
 }
@@ -65,11 +62,12 @@
         [self addRoleMenuList];
         
     }else if([titleStr isEqualToString:@"特效"]){
- 
+        [self addLyfMenuList];
         
     }else if([titleStr isEqualToString:@"技能"]){
         
     }else if([titleStr isEqualToString:@"挂件"]){
+        [self addGujianMenuList];
     }
     
 }
@@ -77,7 +75,7 @@
 {
     if([str isEqualToString:@"清理"]){
         [self.sceneView.scene3D clearAll];
-        [self.sceneView.scene3D addDisplay:[[GridLineSprite alloc]init]];
+        self.mainChar=nil;
     }else if([str isEqualToString:@"网格"]){
         [self.sceneView.scene3D addDisplay:[[GridLineSprite alloc]init]];
     }else if([str isEqualToString:@"拉+"]){
@@ -99,14 +97,8 @@
     [arr addObject:@"2013"];
     [arr addObject:@"2014"];
     [arr addObject:@"2015"];
-    [arr addObject:@"清理"];
-    [arr addObject:@"网格"];
-    [arr addObject:@"拉+"];
-    [arr addObject:@"推-"];
-    [arr addObject:@"返回"];
+    [self addBaseMenuButs:arr];
     [self addButsByArr:arr  action: @selector(selectSceneClikEvent:)];
-    
-    
 }
 - (void) selectSceneClikEvent:(UIButton *) btn
 {
@@ -124,14 +116,8 @@
     [arr addObject:@"50013"];
     [arr addObject:@"50015"];
     [arr addObject:@"yezhuz"];
-    [arr addObject:@"清理"];
-    [arr addObject:@"网格"];
-    [arr addObject:@"拉+"];
-    [arr addObject:@"推-"];
-    [arr addObject:@"返回"];
+    [self addBaseMenuButs:arr];
     [self addButsByArr:arr  action: @selector(selectRoleClikEvent:)];
-    
-    
 }
 - (void) selectRoleClikEvent:(UIButton *) btn
 {
@@ -142,6 +128,88 @@
     [self addRoleToScene:titleStr pos:[[Vector3D alloc]x:0 y:0 z:0]];
 }
 
+-(void)addLyfMenuList;
+{
+    NSMutableArray* arr=[[NSMutableArray alloc]init];
+    [arr addObject:@"levelup"];
+    [arr addObject:@"reviveeff"];
+    [arr addObject:@"10018"];
+    [arr addObject:@"10017"];
+    [arr addObject:@"13012"];
+    [arr addObject:@"diamondseffect"];
+    [self addBaseMenuButs:arr];
+    [self addButsByArr:arr  action: @selector(selectLyfClikEvent:)];
+}
+- (void) selectLyfClikEvent:(UIButton *) btn
+{
+    NSString* titleStr=btn.titleLabel.text;
+    if([self selectBaseButByName:titleStr]){
+        return;
+    }
+    [self playLyfByUrl:[NSString stringWithFormat:@"model/%@_lyf.txt",titleStr]];
+    self.mainChar=[[SceneChar alloc]init];
+    [self.sceneView.scene3D addMovieDisplay:self.mainChar] ;
+    [self.mainChar setRoleUrl: getRoleUrl(@"50001")];
+    [self.mainChar addPart:SceneChar.WEAPON_PART bindSocket:SceneChar.WEAPON_DEFAULT_SLOT url:getModelUrl(@"50011")];
+    [self.sceneView.scene3D.skillManager preLoadSkill:getSkillUrl(@"jichu_1")];
+}
+-(void)addGujianMenuList;
+{
+    NSMutableArray* arr=[[NSMutableArray alloc]init];
+    [arr addObject:@"战士"];
+    [arr addObject:@"武器"];
+    [arr addObject:@"坐骑"];
+    [arr addObject:@"行走"];
+    [arr addObject:@"站立"];
+    [self addBaseMenuButs:arr];
+    [self addButsByArr:arr  action: @selector(selectGuajianClikEvent:)];
+    
+    
+}
+-(void)addBaseMenuButs:(NSMutableArray*)arr{
+    [arr addObject:@"清理"];
+    [arr addObject:@"网格"];
+    [arr addObject:@"拉+"];
+    [arr addObject:@"推-"];
+    [arr addObject:@"返回"];
+}
+- (void) selectGuajianClikEvent:(UIButton *) btn
+{
+    NSString* titleStr=btn.titleLabel.text;
+    if([self selectBaseButByName:titleStr]){
+        return;
+    }
+    
+    if([titleStr isEqualToString:@"战士"]){
+        if(!self.mainChar){
+            self.mainChar=[[SceneChar alloc]init];
+            [self.mainChar setRoleUrl: getRoleUrl(@"50001")];
+            [self.sceneView.scene3D addMovieDisplay:self.mainChar] ;
+        }
+        
+    }else if([titleStr isEqualToString:@"武器"]){
+        if(self.mainChar){
+            [self.mainChar addPart:SceneChar.WEAPON_PART bindSocket:SceneChar.WEAPON_DEFAULT_SLOT url:getModelUrl(@"50011")];
+        }
+        
+    }else if([titleStr isEqualToString:@"坐骑"]){
+        if(self.mainChar){
+            [self.mainChar setMountById:@"5104"];
+        }
+        
+        
+    }else if([titleStr isEqualToString:@"行走"]){
+        if(self.mainChar){
+      
+        }
+        
+    }else if([titleStr isEqualToString:@"站立"]){
+        if(self.mainChar){
+       
+        }
+    }
+  
+}
 -(void)addButsByArr:(NSMutableArray*)arr action:(SEL)action
 {
     self.butItems=[[NSMutableArray alloc]init];
@@ -152,15 +220,6 @@
         [self.butItems addObject:oneBut];
     }
 }
-
--(void)addEventButBy:(NSString*)tittle;
-{
-    UIButton* oneBut=[self makeButtion];
-    [oneBut setTitle:tittle forState:UIControlStateNormal];
-    [oneBut addTarget:self action:@selector(oneButClikEvent:) forControlEvents:UIControlEventTouchUpInside] ;
-    [self.butItems addObject:oneBut];
-}
-
 - (void)viewDidLayoutSubviews
 {
     for (int i=0; i< self.butItems.count; i++) {
@@ -169,153 +228,6 @@
         
         self.butItems[i].frame=CGRectMake(tx*75+10,  CGRectGetMaxY(self.sceneView.frame)+55+ty*50, 60, 30);
     }
-}
-
--(void)showLyfItems;
-{
-    [self clearButs];
-    [self addEventLyfButBy:@"10017"];
-    [self addEventLyfButBy:@"10018"];
-    [self addEventLyfButBy:@"13012"];
-    [self addEventLyfButBy:@"levelup"];
-    [self addEventButBy:@"清理"];
-    [self addEventButBy:@"拉+"];
-    [self addEventButBy:@"推-"];
-    [self viewDidLayoutSubviews];
-    
-}
-
--(void)clearButs;
-{
-    while (self.butItems.count) {
-        [self.butItems[0] removeFromSuperview];
-        [self.butItems removeObjectAtIndex:0];
-        
-    }
-}
--(void)addEventLyfButBy:(NSString*)tittle;
-{
-    UIButton* oneBut=[self makeButtion];
-    [oneBut setTitle:tittle forState:UIControlStateNormal];
-    [oneBut addTarget:self action:@selector(oneButLyfClikEvent:) forControlEvents:UIControlEventTouchUpInside] ;
-    [self.butItems addObject:oneBut];
-}
-- (void) oneButLyfClikEvent:(UIButton *) btn;
-{
-    NSString* lyfStr=btn.titleLabel.text;
-    
-    [self.sceneView.scene3D clearAll];
-    [self.sceneView.scene3D addDisplay:[[GridLineSprite alloc]init]];
-    
-    //            NSMutableArray<NSString*>* lyfItem=[[NSMutableArray alloc]init];
-    //            [lyfItem addObject:@"model/diamondseffect_lyf.txt"];
-    //            [lyfItem addObject:@"model/levelup_lyf.txt"];
-    //            [lyfItem addObject:@"model/reviveeff_lyf.txt"];
-    //            [lyfItem addObject:@"model/10017_lyf.txt"];
-    //            [lyfItem addObject:@"model/10018_lyf.txt"];
-    //            [lyfItem addObject:@"model/13012_lyf.txt"];
-    
-    [self playLyfByUrl:[NSString stringWithFormat:@"model/%@_lyf.txt",lyfStr]];
-}
-
-- (void) oneButClikEvent:(UIButton *) btn
-{
-    
-    NSString* titleStr=btn.titleLabel.text;
-    if([titleStr isEqualToString:@"角色"]){
-        [self makeRollAndMount];
-        [self addRoleToScene:@"yezhuz" pos:[[Vector3D alloc]x:-20 y:0 z:0]];
-        [self addRoleToScene:@"50004" pos:[[Vector3D alloc]x:-40 y:0 z:0]];
-        [self addRoleToScene:@"50005" pos:[[Vector3D alloc]x:-60 y:0 z:0]];
-        [self addRoleToScene:@"50006" pos:[[Vector3D alloc]x:-80 y:0 z:0]];
-    }
-    if([titleStr isEqualToString:@"特效"]){
-        
-        //     Skill* skill= [self.sceneView.scene3D.skillManager getSkill: getSkillUrl(@"jichu_1") name:@"m_skill_01"];
-        [self showLyfItems];
-        
-    }
-    if([titleStr isEqualToString:@"技能"]){
-        
-        if(!self.mainChar){
-            self.mainChar=[[SceneChar alloc]init];
-            [self.sceneView.scene3D addMovieDisplay:self.mainChar] ;
-            //50001
-            [self.mainChar setRoleUrl: getRoleUrl(@"50001")];
-            [self.mainChar addPart:SceneChar.WEAPON_PART bindSocket:SceneChar.WEAPON_DEFAULT_SLOT url:getModelUrl(@"50011")];
-            [self.sceneView.scene3D.skillManager preLoadSkill:getSkillUrl(@"jichu_1")];
-            
-        }else{
-            
-            Skill* skill= [self.sceneView.scene3D.skillManager getSkill: getSkillUrl(@"jichu_1") name:@"m_skill_01"];
-            skill.scene3D=self.sceneView.scene3D;
-            [skill reset];
-            [skill configFixEffect:self.mainChar completeFun:nil posObj:nil ];
-            [self.mainChar playSkill:skill];
-            NSLog(@"播放技能");
-            
-        }
-        
-    }
-    if([titleStr isEqualToString:@"清理"]){
-        [self.sceneView.scene3D clearAll];
-        [self.sceneView.scene3D addDisplay:[[GridLineSprite alloc]init]];
-    }
-    if([titleStr isEqualToString:@"拉+"]){
-        self.sceneView.scene3D.camera3D.distance-=20;
-    }
-    if([titleStr isEqualToString:@"推-"]){
-        self.sceneView.scene3D.camera3D.distance+=20;
-    }
-    if([titleStr isEqualToString:@"新加"]){
-        [self addRoleToScene:@"50001" pos:[[Vector3D alloc]x:-20 y:0 z:0]];
-    }
-    
-    if([titleStr isEqualToString:@"场景"]){
-        [self.sceneView loadSeceneByUrl:@"2012"];
-    }
-    
-    if([titleStr isEqualToString:@"特别"]){
-        [self addRoleToScene:@"50001" pos:[[Vector3D alloc]x:0 y:0 z:0]];
-    }
-    if([titleStr isEqualToString:@"野猪"]){
-        [self addRoleToScene:@"yezhuz" pos:[[Vector3D alloc]x:0 y:0 z:0]];
-    }
-    
-    //
-}
-
--(void)playLyfByUrl:(NSString*)value
-{
-    
-    ParticleManager* particleManager=  self.sceneView.scene3D.particleManager;
-    NSString* modeurl =[[Scene_data default]getWorkUrlByFilePath:value];
-    [[GroupDataManager default] getGroupData:modeurl Block:^(GroupRes *groupRes) {
-        for (int i = 0; i < groupRes.dataAry.count; i++) {
-            GroupItem *item = groupRes.dataAry[i];
-            if (item.types ==SCENE_PARTICLE_TYPE) {
-                CombineParticle*  particle =   [ParticleManager   getParticleByte: item.particleUrl];
-                [particleManager addParticle:particle];
-            } else {
-                NSLog(@"播放的不是单纯特效");
-            }
-        }
-    }];
-}
-/*
- 武器，角色，坐骑
- */
--(void)makeRollAndMount;
-{
-    SceneChar* sc=[[SceneChar alloc]init];
-    [self.sceneView.scene3D addMovieDisplay:sc] ;
-    [sc setRoleUrl: getRoleUrl(@"50002")];
-    [sc setMountById:@"5104"];
-    [sc play:@"walk" completeState:0 needFollow:NO];
-    [sc addPart:SceneChar.WEAPON_PART bindSocket:SceneChar.WEAPON_DEFAULT_SLOT url:getModelUrl(@"50011")];
-    sc.x=50;
-    
-    
 }
 /*
  角色
@@ -343,14 +255,26 @@
     
     return but;
 }
+/*
+ 播放特效
+ */
+  
+-(void)playLyfByUrl:(NSString*)value
+{
 
--(void)MakeUiViewShadow:(UIView *)value size:(CGFloat )size  color:(UIColor *)color   {
-    UIView *myView= value;
-    myView.layer.shadowColor = color.CGColor;
-    myView.layer.shadowOffset = CGSizeMake(5,5);; //[水平偏移, 垂直偏移]
-    myView.layer.shadowOpacity = 0.2f; // 0.0 ~ 1.0 的值
-    myView.layer.shadowRadius = size; // 阴影发散的程度
-    myView.layer.masksToBounds = NO;
+    ParticleManager* particleManager=  self.sceneView.scene3D.particleManager;
+    NSString* modeurl =[[Scene_data default]getWorkUrlByFilePath:value];
+    [[GroupDataManager default] getGroupData:modeurl Block:^(GroupRes *groupRes) {
+        for (int i = 0; i < groupRes.dataAry.count; i++) {
+            GroupItem *item = groupRes.dataAry[i];
+            if (item.types ==SCENE_PARTICLE_TYPE) {
+                CombineParticle*  particle =   [ParticleManager   getParticleByte: item.particleUrl];
+                [particleManager addParticle:particle];
+            } else {
+                NSLog(@"播放的不是单纯特效");
+            }
+        }
+    }];
 }
-
 @end
+
