db6d8ace2317f214abe73f2bcf7b13390ee1f17a
pan3dme <3423526@qq.com>
2020-11-18 23:14:27 +0800

    no message

---
+++ test/two/app/src/main/java/com/example/two/HelloWorld.java
@@ -0,0 +1,24 @@
+package com.example.two;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.opengl.GLSurfaceView;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.lang.reflect.Field;
+import java.util.Random;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class HelloWorld extends Activity {
+
+
+}
