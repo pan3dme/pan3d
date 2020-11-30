04232e25cc85ec9aa9d0b19b77c56f8915afb767
pan3dme <3423526@qq.com>
2020-11-30 20:08:15 +0800

    no message

--- andriodGL/app/src/main/java/scene/Test001.java
+++ andriodGL/app/src/main/java/scene/Test001.java
@@ -62,66 +62,19 @@ public class Test001 extends AppCompatActivity   {
 
 
     private static final String TAG ="SceneLyfBase" ;
-
     FragmentFirstBinding binding;
-
-
     private MyViewModel myViewModel;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_first);
         Log.d(TAG, "onCreate: ");
-
-
         binding= DataBindingUtil.setContentView(this,R.layout.fragment_first);
         myViewModel=  ViewModelProviders.of(this).get(MyViewModel.class);
         binding.setData(myViewModel);
         binding.setLifecycleOwner(this);
 
-
-
-
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
 
-        Log.d(TAG, "onStart: ");
-
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d(TAG, "onResume: ");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d(TAG, "onPause: ");
-
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d(TAG, "onStop: ");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.d(TAG, "onRestart: ");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d(TAG, "onDestroy: ");
-    }
 }
