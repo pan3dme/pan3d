6cfa0dc0e9610ec50249ffa0bf7562982c5e7b30
pan3dme <3423526@qq.com>
2021-01-12 14:16:04 +0800

    月

--- h5gl/new/src/pan3d/core/ColorTransition.ts
+++ h5gl/new/src/pan3d/core/ColorTransition.ts
@@ -13,39 +13,51 @@ module Pan3d {
         }
 
         public constructor() {
-            this._canvas = document.createElement("canvas");
-            this._cxt = this._canvas.getContext("2d");
-            this._gnt = this._cxt.createLinearGradient(0, 0, 128, 0);
-            this._canvas.style.zIndex = "1";
+            // this._canvas = document.createElement("canvas");
+            // this._cxt = this._canvas.getContext("2d");
+            // this._gnt = this._cxt.createLinearGradient(0, 0, 128, 0);
+            // this._canvas.style.zIndex = "1";
             //document.body.appendChild(this._canvas);
         }
 
         public getImageData($data: any): ImageData {
-            var length: number = $data.pos.length;
-            var color: Vector3D = new Vector3D();
-            for (var i: number = 0; i < length; i++) {
-                hexToArgb($data.color[i], false, color);
-                this._gnt.addColorStop($data.pos[i] / 255, 'rgba(' + color.x + ',' + color.y + ',' + color.z + ',' + $data.alpha[i] + ')');
+            // console.log($data);
+            // var length: number = $data.pos.length;
+            // var color: Vector3D = new Vector3D();
+            // for (var i: number = 0; i < length; i++) {
+            //     hexToArgb($data.color[i], false, color);
+            //     this._gnt.addColorStop($data.pos[i] / 255, 'rgba(' + color.x + ',' + color.y + ',' + color.z + ',' + $data.alpha[i] + ')');
+            // }
+            // this._cxt.fillStyle = this._gnt;
+            // this._cxt.fillRect(0, 0, 128, 2);
+            // return this._cxt.getImageData(0, 0, 128, 2);
+
+            var imgData: ImageData = new ImageData(128,1);
+            var index: number;
+            for (var i: number = 0; i < imgData.width; i++) {
+                index = i * 4;
+                imgData.data[index] = 1;
+                imgData.data[index + 1] =0;
+                imgData.data[index + 2] = 0;
+                imgData.data[index + 3] = 1;
             }
-            this._cxt.fillStyle = this._gnt;
-            this._cxt.fillRect(0, 0, 128, 2);
-            return this._cxt.getImageData(0, 0, 128, 2);
+            return imgData;
         }
 
         public getImageDataByVec($data: any, $lenght: number): ImageData {
-            var imgData: ImageData = this._cxt.createImageData(64, 1);
+            var imgData: ImageData = new ImageData(64,1);
             var index: number;
             var baseindex: number;
-            for (var i: number = 0; i < 64; i++) {
+            for (var i: number = 0; i < imgData.width; i++) {
                 index = i * 4;
-                baseindex = float2int(i / 64 * $lenght) * 4;
+                baseindex = float2int(i / imgData.width * $lenght) * 4;
                 imgData.data[index] = $data[baseindex];
                 imgData.data[index + 1] = $data[baseindex + 1];
                 imgData.data[index + 2] = $data[baseindex + 2];
                 imgData.data[index + 3] = $data[baseindex + 3];
             }
             return imgData;
-            //Scene_data.context3D.getTexture(imgData);
+ 
         }
 
         public setData() {
