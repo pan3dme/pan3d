70015fe4418abaec0248c2f7710495b098d65cd8
pan3dme <3423526@qq.com>
2021-01-17 12:03:42 +0800

    no message

---
+++ metalios/base/base/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  base
+//
+//  Created by pan3dme on 2021/1/17.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
