8c17c83ce7e396b92b58b73f6de47a98e78bddd0
pan3dme <3423526@qq.com>
2021-04-07 13:42:07 +0800

    no message

--- metalios/iosgl/iosgl/view/home/ListViewController.m
+++ metalios/iosgl/iosgl/view/home/ListViewController.m
@@ -41,7 +41,7 @@
 }
 -(void)loadXmlByUrl
 {
-    NSString* netUrl = @"https://webpan.oss-cn-shanghai.aliyuncs.com/res/pan/test/iosmetia/jason10.xml";
+    NSString* netUrl = @"https://webpan.oss-cn-shanghai.aliyuncs.com/res/pan/test/iosmetia/jason11.xml";
  
     [[LoadManager default] loadUrl:netUrl type:LoadManager.XML_TYPE fun:^(NSString* value) {
         NSDictionary* dic=(NSDictionary*)value;
