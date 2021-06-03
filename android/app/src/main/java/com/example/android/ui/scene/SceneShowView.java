8ee63c0f2642343523cb58ddfc00f00b98274d88
pan3dme <3423526@qq.com>
2021-06-03 18:17:02 +0800

    no message

--- android/app/src/main/java/com/example/android/ui/scene/SceneShowView.java
+++ android/app/src/main/java/com/example/android/ui/scene/SceneShowView.java
@@ -1,11 +1,16 @@
 package com.example.android.ui.scene;
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.solver.widgets.Rectangle;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
+
+import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Display;
@@ -24,10 +29,110 @@ import org.json.JSONObject;
  * A simple {@link Fragment} subclass.
  * create an instance of this fragment.
  */
-public class SceneShowView extends AppCompatActivity implements GestureDetector.OnGestureListener {
+public class SceneShowView extends AppCompatActivity   {
     private static final String TAG ="SceneShowView" ;
     ConstrainSceneView constrainSceneViewOne;
-    GestureDetector detector;
+
+
+    /**
+     * 初始化状态常量
+     */
+    public static final int STATUS_INIT = 1;
+
+    /**
+     * 图片放大状态常量
+     */
+    public static final int STATUS_ZOOM_OUT = 2;
+
+    /**
+     * 图片缩小状态常量
+     */
+    public static final int STATUS_ZOOM_IN = 3;
+
+    /**
+     * 图片拖动状态常量
+     */
+    public static final int STATUS_MOVE = 4;
+
+    /**
+     * 用于对图片进行移动和缩放变换的矩阵
+     */
+    private Matrix matrix = new Matrix();
+
+    /**
+     * 待展示的Bitmap对象
+     */
+//    private Bitmap sourceBitmap;
+
+    /**
+     * 记录当前操作的状态，可选值为STATUS_INIT、STATUS_ZOOM_OUT、STATUS_ZOOM_IN和STATUS_MOVE
+     */
+    private int currentStatus;
+
+    /**
+     * ZoomImageView控件的宽度
+     */
+    private int width;
+
+    /**
+     * ZoomImageView控件的高度
+     */
+    private int height;
+
+    /**
+     * 记录两指同时放在屏幕上时，中心点的横坐标值
+     */
+    private float centerPointX;
+
+    /**
+     * 记录两指同时放在屏幕上时，中心点的纵坐标值
+     */
+    private float centerPointY;
+
+
+
+    /**
+     * 记录上次手指移动时的横坐标
+     */
+    private float lastXMove = -1;
+
+    /**
+     * 记录上次手指移动时的纵坐标
+     */
+    private float lastYMove = -1;
+
+    /**
+     * 记录手指在横坐标方向上的移动距离
+     */
+    private float movedDistanceX;
+
+    /**
+     * 记录手指在纵坐标方向上的移动距离
+     */
+    private float movedDistanceY;
+
+    /**
+     * 记录图片在矩阵上的横向偏移值
+     */
+    private float totalTranslateX;
+
+    /**
+     * 记录图片在矩阵上的纵向偏移值
+     */
+    private float totalTranslateY;
+
+    /**
+     * 记录图片在矩阵上的总缩放比例
+     */
+    private float totalRatio;
+
+
+
+
+    /**
+     * 记录上次两指之间的距离
+     */
+    private double lastFingerDis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,13 +141,7 @@ public class SceneShowView extends AppCompatActivity implements GestureDetector.
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle(getString(R.string.app_name));
         String sceneinfo = getIntent().getStringExtra("sceneinfo");
-
-        detector = new GestureDetector(this, this);
-        //实例化这个手势检测器对象
-
         Context context=  getApplicationContext();
-
-
         final ConstraintLayout constraintlayout =  findViewById(R.id.base_scene_gl_view);
 
 
@@ -51,7 +150,8 @@ public class SceneShowView extends AppCompatActivity implements GestureDetector.
             public void StateChange(boolean State) {
                 initSceneDataByBunld(sceneinfo);
 
-
+                totalTranslateY= -   constrainSceneViewOne.mainScene3D.camera3D.rotationX*4;
+                totalTranslateX=-  constrainSceneViewOne.mainScene3D.camera3D.rotationY;
 
             }
         });
@@ -64,16 +164,110 @@ public class SceneShowView extends AppCompatActivity implements GestureDetector.
         constrainSceneViewOne.setRect(rectangle);
 
 
+
+
+
+
+
+    }
+
+    /**
+     * 计算两个手指之间的距离。
+     *
+     * @param event
+     * @return 两个手指之间的距离
+     */
+    private double distanceBetweenFingers(MotionEvent event) {
+        float disX = Math.abs(event.getX(0) - event.getX(1));
+        float disY = Math.abs(event.getY(0) - event.getY(1));
+        return Math.sqrt(disX * disX + disY * disY);
+    }
+
+
+    private void move( ) {
+        matrix.reset();
+        // 根据手指移动的距离计算出总偏移值
+        float translateX = totalTranslateX + movedDistanceX;
+        float translateY = totalTranslateY + movedDistanceY;
+        // 先按照已有的缩放比例对图片进行缩放
+        matrix.postScale(totalRatio, totalRatio);
+        // 再根据移动距离进行偏移
+        matrix.postTranslate(translateX, translateY);
+        totalTranslateX = translateX;
+        totalTranslateY = translateY;
+
+        this.constrainSceneViewOne.mainScene3D.camera3D.rotationX=-totalTranslateY/4;
+        this.constrainSceneViewOne.mainScene3D.camera3D.rotationY=-totalTranslateX;
+
+
     }
     //下面实现的这些接口负责处理所有在该Activity上发生的触碰屏幕相关的事件
     @Override
-    public boolean onTouchEvent(MotionEvent e)
+    public boolean onTouchEvent(MotionEvent event)
     {
-        return detector.onTouchEvent(e);
+        switch (event.getActionMasked()) {
+            case MotionEvent.ACTION_POINTER_DOWN:
+                if (event.getPointerCount() == 2) {
+                    // 当有两个手指按在屏幕上时，计算两指之间的距离
+                    lastFingerDis = distanceBetweenFingers(event);
+                }
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if (event.getPointerCount() == 1) {
+                    // 只有单指按在屏幕上移动时，为拖动状态
+                    float xMove = event.getX();
+                    float yMove = event.getY();
+                    if (lastXMove == -1 && lastYMove == -1) {
+                        lastXMove = xMove;
+                        lastYMove = yMove;
+                    }
+                    currentStatus = STATUS_MOVE;
+                    movedDistanceX = xMove - lastXMove;
+                    movedDistanceY = yMove - lastYMove;
+
+                    move( );
+
+                    lastXMove = xMove;
+                    lastYMove = yMove;
+                } else if (event.getPointerCount() == 2) {
+                    // 有两个手指按在屏幕上移动时，为缩放状态
+                    centerPointBetweenFingers(event);
+                    double fingerDis = distanceBetweenFingers(event);
+                    this.constrainSceneViewOne.mainScene3D.camera3D.distance+=(float)lastFingerDis-(float) fingerDis;
+
+                    lastFingerDis = fingerDis;
+                }
+                break;
+            case MotionEvent.ACTION_POINTER_UP:
+                if (event.getPointerCount() == 2) {
+                    lastXMove = -1;
+                    lastYMove = -1;
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                // 手指离开屏幕时将临时值还原
+                lastXMove = -1;
+                lastYMove = -1;
+                break;
+            default:
+                break;
+        }
+        return true;
     }
-    public Display getDisplay(Activity activity){
-        return activity.getWindowManager().getDefaultDisplay();
+    /**
+     * 计算两个手指之间中心点的坐标。
+     *
+     * @param event
+     */
+    private void centerPointBetweenFingers(MotionEvent event) {
+        float xPoint0 = event.getX(0);
+        float yPoint0 = event.getY(0);
+        float xPoint1 = event.getX(1);
+        float yPoint1 = event.getY(1);
+        centerPointX = (xPoint0 + xPoint1) / 2;
+        centerPointY = (yPoint0 + yPoint1) / 2;
     }
+
     private void initSceneDataByBunld(String val)
     {
 
@@ -144,8 +338,6 @@ public class SceneShowView extends AppCompatActivity implements GestureDetector.
 
     }
 
-
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
@@ -154,46 +346,5 @@ public class SceneShowView extends AppCompatActivity implements GestureDetector.
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public boolean onDown(MotionEvent e) {
-        return false;
-    }
 
-    @Override
-    public void onShowPress(MotionEvent e) {
-
-    }
-
-    @Override
-    public boolean onSingleTapUp(MotionEvent e) {
-        return false;
-    }
-
-    @Override
-    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-        Log.d(TAG,   "The method has been called - onScroll");
-        //当该方法被调用时，通过一个Toast来提示用户哪个方法被调用了，下同
-
-
-        this.constrainSceneViewOne.mainScene3D.camera3D.rotationX+=distanceY*0.1;
-        this.constrainSceneViewOne.mainScene3D.camera3D.rotationY+=distanceX*0.1;
-
-        if(distanceY >= 0){
-
-        }
-        else{
-
-        }
-        return false;
-    }
-
-    @Override
-    public void onLongPress(MotionEvent e) {
-
-    }
-
-    @Override
-    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-        return false;
-    }
 }
